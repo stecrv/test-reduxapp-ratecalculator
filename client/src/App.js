225aa88346dac5b1b1c16795f8839bbbd00d3272
stecrv <stefano.caravana@gmail.com>
2017-06-24 16:20:29 +0100

    Istructions added + style

--- client/src/App.js
+++ client/src/App.js
@@ -13,14 +13,20 @@ class App extends Component {
           <h1 className="fmz-white-font">Finimize Interest Rate Calculator</h1>
         </div>
 				<div className="financial-inputs">
-					<p className="input-label">How much have you saved?</p>
-					<CurrencyInput defaultValue={0}/>
+					<div className="inline oneThird">
+                        <p className="input-label">How much have you saved?</p>
+                        <CurrencyInput defaultValue={0}/>
+					</div>
+					<div className="inline oneThird">
+                        <p className="input-label">How much will you save each month?</p>
+                        <CurrencyInput defaultValue={0}/>
+					</div>
+                    <div className="inline oneThird">
+                        <p className="input-label">How much interest will you earn per year?</p>
+                        <SliderInput defaultValue={4}/>
+					</div>
+                    <Instructions />
 
-					<p className="input-label">How much will you save each month?</p>
-					<CurrencyInput defaultValue={0}/>
-
-					<p className="input-label">How much interest will you earn per year?</p>
-					<SliderInput defaultValue={4}/>
 				</div>
 				<div className="financial-display">
 					{/*We have included some sample data here, you will need to replace this
@@ -44,9 +50,7 @@ class App extends Component {
 						}
 					]}/>
 				</div>
-		  		<div>
-					<Instructions />
-				</div>
+
       </div>
     );
   }
